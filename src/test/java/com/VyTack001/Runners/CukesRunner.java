495ffbe50c1309731cc4ce9d5dbe289fb5c8c426
orhanturan3898156 <orhanturan1566@gmail.com>
2020-07-22 16:22:27 -0400

    new project

---
+++ src/test/java/com/VyTack001/Runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.VyTack001.Runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        //plugin,feature, tags,dryRun,glue
+        plugin = {"html:target/cucumber-report.html", "json:target/json-report.json",
+                "rerun:target/rerun-report.txt"},
+
+        features = "src/test/resources/features",
+        glue = "com/VyTack001/StepDefinitions",
+        dryRun = false,
+        tags = ""
+
+
+)
+
+
+public class CukesRunner {
+}
---
+++ src/test/java/com/VyTack001/Runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.VyTack001.Runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+
+)
+
+public class FailedTestRunner {
+}
