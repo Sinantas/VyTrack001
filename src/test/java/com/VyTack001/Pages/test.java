243bfdeb1ab62ab1d0afa55b62941734c25490b3
Sinantas <sinantasfl@gmail.com>
2020-07-23 08:19:00 -0400

    commit

--- src/test/java/com/VyTack001/Pages/test.java
+++ src/test/java/com/VyTack001/Pages/test.java
@@ -1,4 +1,12 @@
 package com.VyTack001.Pages;
 
 public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("First project");
+        System.out.println("Second project");
+
+
+    }
 }
